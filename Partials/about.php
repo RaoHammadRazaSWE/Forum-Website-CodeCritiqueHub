79fa04bc9320640365f401e19a6c1909f275a2ec
Rao Hammad Raza <145425438+RAOHammadraza9443@users.noreply.github.com>
2024-04-02 00:42:35 +0500

    Create about.php

---
+++ Partials/about.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome to iDiscuss - Coding Forums</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php include '_dbconnect.php';?>
+
+    <?php include '_header.php';?>
+
+    <div class="media my-4">
+        <a href="https://imgbb.com/">
+            <img src="https://i.ibb.co/GTrS4MN/profile-pic-1.png" alt="profile-pic-1" class="mx-auto d-block"
+                style="width: 150px; height: auto;" />
+        </a>
+    </div>
+
+    <!-- Content -->
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="about-container">
+                    <h1 class="center-text">Alhamdulillah My First Website Done.</h1>
+                    <P> </P>
+                    <h2 class="heading">About <span>Me</span></h2>
+                    <p>
+                        🚀 Greetings World! I'm Rao Hammad Raza, a Computer Engineering Graduate from BZU, Multan in
+                        (PAK), with an insatiable curiosity for technology. Witnessing the transformative power, it
+                        wields in our world never fails to captivate me. As a fervent frontend developer, I embark on a
+                        journey into the ever-evolving realm of web development armed with a repertoire of cutting-edge
+                        technologies.
+                    </p>
+
+                    <p>👨‍💻 Skills:</p>
+                    <p>My forte lies in crafting captivating and responsive web experiences utilizing Next.js,
+                        React.js, Node.js, and TypeScript. These tools not only ignite my passion but also enable me to
+                        seamlessly translate concepts into pixel-perfect, user-centric interfaces. My proficiency
+                        extends
+                        to a spectrum of disciplines including Data Structures and Algorithms (C++), Web Development
+                        (MYSQL), PHP, MS Office, and Machine Learning (Python). Where my skill set may fall short, my
+                        unwavering determination to learn propels me forward.</p>
+
+                    <p>🌐 Responsive Design</p>
+                    <p>Aficionado: I am dedicated to ensuring that websites adapt seamlessly to diverse screen sizes. My
+                        ethos revolves around crafting not merely websites, but immersive and accessible digital
+                        ecosystems
+                        tailored to meet the varied needs of users.</p>
+
+                    <p>🌱 Continuous Learner:</p>
+                    <p>Embracing the role of a newcomer in the industry, I thrive on the thrill of continuous learning
+                        and growth. Each project presents an opportunity for honing my skills and exploring innovative
+                        solutions. The dynamic landscape of frontend development keeps me engaged, and I eagerly embrace
+                        the challenge it presents. 🛠️ Tech Stack: Proficient in Next.js, React.js, Node.js, and
+                        TypeScript, I harness these tools to construct robust and scalable applications. My ambition is
+                        to
+                        contribute to projects that push the boundaries of web development's potential.</p>
+
+                    <p>🤝 Collaborative Spirit:</p>
+                    <p>Beyond being a coder, I am a staunch advocate for teamwork and collaboration. Effective
+                        communication and synergy lie at the heart of my approach, ensuring that the end product not
+                        only
+                        meets but surpasses expectations.</p>
+
+                    <p>🔗 Connect with Me:</p>
+                    <p>Thrilled to connect with fellow developers, designers, and enthusiasts alike! Let's pool our
+                        talents to create something extraordinary and elevate the digital landscape together.</p>
+
+                    <p>📽️ Project:</p>
+                    <p>Embarking on my journey, I'm continuously growing my portfolio. Explore completed projects like
+                        an
+                        AI Chatbot, Amazon Website Clone, Hugging Face Chatbot Model, and Currency Converter on my
+                        GitHub, with more to come!</p>
+
+                    <p>🗺️ Beyond Technology:</p>
+                    <p>I find solace in cricket and derive immense joy from my passion for agriculture, which I
+                        cultivated
+                        during my college days.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer -->
+
+    <?php include '_footer.php';?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
